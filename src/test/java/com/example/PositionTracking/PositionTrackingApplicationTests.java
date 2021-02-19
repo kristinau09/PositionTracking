2134fa4c65e6b7136c698c62a86d91947a1d5c92
kristinau09 <kristinau09@gmail.com>
2021-02-19 17:11:30 -0600

    Microservice to track the position of the vehicle

---
+++ src/test/java/com/example/PositionTracking/PositionTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PositionTracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PositionTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
